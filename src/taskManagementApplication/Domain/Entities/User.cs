690fa9bb390c48783c12b74f29e2cd246749d28b
sadullahcihan <sadullah.cihan@gmail.com>
2025-01-11 13:44:37 +0300

    +

--- src/taskManagementApplication/Domain/Entities/User.cs
+++ src/taskManagementApplication/Domain/Entities/User.cs
@@ -5,7 +5,6 @@ using NArchitecture.Core.Persistence.Repositories;
 namespace Domain.Entities;
 public class User : Entity<Guid>
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
